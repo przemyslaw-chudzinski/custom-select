d3686b2f2b111f99a5f43d8c62eb7fdebbd129ac
Przemysław Chudziński <przemyslaw-chudzinski@wp.pl>
2019-05-13 17:19:35 +0200

    new settings added

--- src/default-config.js
+++ src/default-config.js
@@ -1,10 +1,13 @@
 // Default Config
 module.exports = {
-
+    /**
+     * @desc Return template string of the singular option
+     * @param option
+     * @returns {string}
+     */
     templateFn(option) {
         return option.innerText;
     },
-
     /**
      * @desc
      * @param values Array<any>
@@ -13,9 +16,23 @@ module.exports = {
     placeholderTplFn(values) {
         return values.map(val => val.innerText).join(', ');
     },
-
+    /**
+     * @desc Define custom default placeholder
+     * @returns {string}
+     */
+    defaultPlaceholderFn() {
+      return '-- choose any element --'
+    },
+    /**
+     * @desc Decide to bind to the backdrop click event or not
+     */
     closeOnBackdropClick: true,
-
-    hasBackdrop: true
-
+    /**
+     * @desc Decide to show the backdrop or not
+     */
+    hasBackdrop: true,
+    /**
+     * @desc Decide to show the clear all button
+     */
+    showClearAllButton: true
 };
